7224267f8f047f33b37397faeb7c79b6ab9269d5
eggfree <jeremy@chungepstein.com>
2018-04-11 15:00:01 -0400

    completed basic hello world on creating compiling and loading a custom visual. Next step will be composing basic markup for what i want happening and seeing if it renders or works

---
+++ navigation vis/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var myVisualName856B6B155F644C1CA89C1981472F3414_DEBUG = {
+        name: 'myVisualName856B6B155F644C1CA89C1981472F3414_DEBUG',
+        displayName: 'My Visual name',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.11.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.myVisualName856B6B155F644C1CA89C1981472F3414.Visual(options),
+        custom: true
+    };
+}
